d35831d839b324fcc8cf1f6990848d480e01b7b2
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-19 13:42:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://static.vecteezy.com/system/resources/previews/013/296/520/original/lock-locked-school-blue-dotted-line-line-icon-free-vector.jpg" type="image/x-icon">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="css/style.css">
+    <title>Password Generator</title>
+</head>
+
+<body>
+    <div class="container my-5 p-5 border border-info rounded">
+        <h1 class="fw-bold text-center">STRONG PASSWORD GENERATOR</h1>
+    </div>
+</body>
+
+</html>
