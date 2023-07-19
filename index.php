eec4904065d0ee1f30d6a7a2576015739d968155
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-19 13:46:16 +0200

    added form adn some style

--- index.php
+++ index.php
@@ -12,7 +12,13 @@
 
 <body>
     <div class="container my-5 p-5 border border-info rounded">
-        <h1 class="fw-bold text-center">STRONG PASSWORD GENERATOR</h1>
+        <h1 class="fw-bold text-center mb-5">STRONG PASSWORD GENERATOR</h1>
+        <form action="#">
+            <hr>
+            <label for="maxLength">Max characters</label><br>
+            <input class="input-control" type="number" name="maxLength" id="maxLength">
+            <hr>
+        </form>
     </div>
 </body>
 
