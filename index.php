3aaa29d56df3d619fde40896a85237da63df29ab
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-19 14:54:46 +0200

    moved result to dedicated file

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-$max_length = intval($_GET['maxLength']) ?? 0;
-require __DIR__ . './includes/functions/getRandomString.php'
+$max_length = intval($_GET['maxLength'] ?? 0);
+require __DIR__ . './includes/functions/getRandomString.php';
+
+session_start();
+
+$_SESSION['password'] = getRandomString($max_length);
 ?>
 
 <!DOCTYPE html>
@@ -11,19 +15,13 @@ require __DIR__ . './includes/functions/getRandomString.php'
 <body>
     <div class="container my-5 p-5 border border-info rounded">
         <h1 class="fw-bold text-center mb-5">STRONG PASSWORD GENERATOR</h1>
-        <form action="">
+        <form action="result.php">
             <hr>
             <label for="maxLength">Max characters</label><br>
             <input class="input-control" type="number" name="maxLength" id="maxLength"><br>
             <input class="btn btn-primary mt-3" type="submit" value="GENERA!">
             <hr>
         </form>
-        <?php if ($max_length) : ?>
-            <div class="result">
-                <h4 class="mb-4">Password generata:</h4>
-                <p class="password"><?= getRandomString($max_length) ?></p>
-            </div>
-        <?php endif ?>
     </div>
 </body>
 
--- result.php
+++ result.php
@@ -1,10 +1,17 @@
+<?php session_start() ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <?php require './includes/templates/head.php' ?>
 
 <body>
-
+    <div class="container my-5 border border-info p-5">
+        <div class="result text-center">
+            <h4 class="mb-4">Password generata:</h4>
+            <p class="password"><?= $_SESSION['password'] ?></p>
+        </div>
+    </div>
 </body>
 
 </html>
