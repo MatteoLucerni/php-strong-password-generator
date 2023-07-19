2d06ce956aeecb3ba43eee4b3be1de215ada20f5
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-19 15:38:13 +0200

    added link to homepage

--- result.php
+++ result.php
@@ -6,11 +6,12 @@
 <?php require __DIR__ . '/includes/templates/head.php' ?>
 
 <body>
-    <div class="container my-5 border border-info p-5">
-        <div class="result text-center">
+    <div class="container my-5 border border-info p-5 text-center">
+        <div class="result">
             <h4 class="mb-4">Password generata:</h4>
             <p class="password"><?= $_SESSION['password'] ?></p>
         </div>
+        <a class="btn btn-primary mt-5" href="index.php">Torna alla home</a>
     </div>
 </body>
 
