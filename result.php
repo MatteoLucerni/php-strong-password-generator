2837a52b3da2c34caf67e4a2571f36eb35cb35b8
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-19 18:12:32 +0200

    changed titles language

--- result.php
+++ result.php
@@ -9,10 +9,10 @@
 <body>
     <div class="container my-5 border border-info p-5 text-center">
         <div class="result">
-            <h4 class="mb-4">Password generata:</h4>
+            <h4 class="mb-4">Password:</h4>
             <p class="password"><?= $_SESSION['password'] ?></p>
         </div>
-        <a class="btn btn-primary mt-5" href="index.php">Torna alla home</a>
+        <a class="btn btn-primary mt-5" href="index.php">Go back to home</a>
     </div>
 </body>
 
